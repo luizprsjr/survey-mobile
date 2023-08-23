ad417788695809223fb4fcd93ca3e25889855cdd
Luiz Pires <lpiresjunior@gmail.com>
2023-08-23 08:40:11 -0300

    fix: import components with relative paths

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@main': './src/main',
+            '@domain': './src/domain',
+            '@presentation': './src/presentation',
+            '@data': './src/screens',
+            '@infra': './src/storage',
+          },
+        },
+      ],
+    ],
+  }
+}
